c8faaba8d9d6ed4e96447a77c34849c0a2bda0ec
brennank14 <brennank14@gmail.com>
2020-07-08 10:45:59 -0500

    ignore

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,5 @@
 class Player < ActiveRecord::Base
     belongs_to :coach
     belongs_to :position
-
-    def add_player(coach, position)
-        self.create(:coach => coach, :position => position)
-    end
-    
     
 end
